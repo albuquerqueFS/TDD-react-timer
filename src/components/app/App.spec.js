5380b6b379061ce52d01dd17486972ee13544463
Henrique Albuquerque <henrique_barretto@outlook.com>
2021-02-19 00:01:53 -0300

    test: ensure App component renders Timer component

--- src/components/app/App.spec.js
+++ src/components/app/App.spec.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './app';
+import Timer from '../timer/Timer';
 
 describe('App', () => {
+  let container;
+
+  beforeEach(() => (container = shallow(<App />)))
+
   it('should render a <div />', () => {
     const container = shallow(<App />);
     expect(container.find('div').length).toEqual(1);
+  })
+
+  it('should render the Timer Component', () => {
+    expect(container.containsMatchingElement(<Timer />)).toEqual(true)
   })
 })
